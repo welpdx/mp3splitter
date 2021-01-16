13007bd299ddbdc80640f50db2f05a29c2e0dfef
Joe Fresh <jggsan2@gmail.com>
2021-01-15 23:28:10 -0700

    Summary of changes so far:  - Check if ffmpeg is in directory.  - Replace invalid characters in markers, so files can be created ni windows env  - Added Special Case 1. If there's only one output file, just rename that one       file to book name  - Changed some variable names  - Output all in a single folder! subdir = bookname  - Output file naming now: {num_seq}_{marker information}_({bookname}).mp3

--- splitter.py
+++ splitter.py
@@ -7,12 +7,27 @@ import xml.etree.ElementTree as ET
 import os
 import shutil
 '''
-Removed unneccesary comments.
-Revamped command list creation.
-Don't forget to put ffmpeg in folder you welp dumbass!
-Added function to check if ffmpeg is in directory.
+Added code to do the following:
+ - Check if ffmpeg is in directory.
+ - Replace invalid characters in markers, so files can be created ni windows env
+ - Added Special Case 1. If there's only one output file, just rename that one
+      file to book name
+ - Changed some variable names
+ - Output all in a single folder! subdir = bookname
+ - Output file naming now: {num_seq}_{marker information}_({bookname}).mp3
+                       ie: 08_Screwtape_Proposes_a_Toast_(Some_Everyday_Thoughts)
 
+Todo:
+    1. A better way to customize file names? We got segment[0] from
+        build_segments() and the rest in split_file()
 '''
+
+# Sequential number in beginnging (displays mp3 output files in order)
+num_seq = 0
+
+# For Special Case 1.
+is_SingleInputFile = False
+is_SingleOutputFile = False
 
 # Converts time (in secs) to a hh:mm:ss.### format
 def convert_time(time_secs):
@@ -23,16 +38,25 @@ def convert_time(time_secs):
     return f"{hour:02}:{min:02}:{sec:02}.{fraction:03}"
 
 # Get OverDrive MediaMarkers, seperates each marker with it's time.
-def build_segments(filename):
-    audio = eyed3.load(filename)
+def build_segments(mp3file_path):
+    audio = eyed3.load(mp3file_path)
     end_time = convert_time(audio.info.time_secs)
     x = audio.tag.user_text_frames
     xmltext = x.get("OverDrive MediaMarkers").text
     print("OverDrive MediaMarkers: " , xmltext)
     markers = ET.fromstring(xmltext)
-    base_chapter = "invalid I hope I never have chapters like this"
-    num_seq = 0
+
+    # init
+    global num_seq
+    global is_SingleInputFile
+    global is_SingleOutputFile
     segments = []
+
+    # (Special Case 1) If only 1 marker and if only 1 file, it is a single output file
+    if (len(markers) == 1 and is_SingleInputFile == True) :
+        is_SingleOutputFile = True
+
+
     for marker in markers:
         # chapters can be split into several shorter sections and end up being
         # shown like this:
@@ -44,17 +68,21 @@ def build_segments(filename):
         #    Chapter 1-01      03:21.507
         #    Chapter 1-02      06:27.227
         name = marker[0].text.strip()
+
+
+        # FILE NAME FORMAT For Last Half of the : _#_<filename>    NamingTag
+        name = f'{num_seq:02d} {name}'
+
         # Replaces characters that are prohibited in Windows File Name Conventions
         name = name.replace(":", "-")
         name = name.replace("/", "_")
-        if not name.startswith(base_chapter):
-            base_chapter = name
+        name = name.replace('"', "")
+        # Changes spaces to underscore.  (Personal preference)
+        name = name.replace("  ", "_")  # Double Spaces converts to Single underscore first.
+        name = name.replace(" ", "_")   # Then Single Spaces to single underscore
 
-        # if num_seq != 0 show 0
-        name = f'{"_" + (str(num_seq) if num_seq!=0 else "0") + "_"   }{base_chapter}'
         num_seq += 1
         start_time =  marker[1].text
-        name = name.replace(" ", "_")
         segments.append((name, start_time))
     return end_time, segments
 
@@ -70,35 +98,35 @@ def complete_segments(segments, final_time):
         print((segment[0], segment[1], end_time))
     return new_segments
 
-def split_file(filename, segments):
-    print("filename: ", filename)
-    fn = pathlib.Path(filename)
-    fname = fn.stem
-    # removes last 11 digits. inclding
-    fname = fname[:-7]
-    subdir = pathlib.Path(fname)
-    # Gives user a warning before deleting existing folder.
-    if os.path.exists(subdir):
-        # input("Folder exists already. Press enter to continue.")
-        shutil.rmtree(subdir)
-    else:
-        subdir.mkdir()
 
-    print("subdir: ",subdir)
+def split_file(mp3file_path, segment, bookname):
+
+    # Init.
+    global is_SingleOutputFile
+
+    fn = pathlib.Path(mp3file_path)
+
+    subdir = bookname
+
+    # Remove Spaces in bookname, Which if the first part of the final file name
+    bookname = bookname.replace("  ", "_")  # Double Spaces converts to Single underscore first.
+    bookname = bookname.replace(" ", "_")   # Then Single Spaces to single underscore
+
+
     for segment in segments:
-        print("segments: ",segments)
-        print("segment: ",segment)
 
-        segname = f"{subdir}/{fname}_{segment[0]}{fn.suffix}"
-        print("segname: ",segname)
-        # GOT IT. It is the command = cmd.split() that split it all based on "spaces". We need to split cmd and add ["ffmpeg", "-i", "asdf123"] in front into command.
+        if is_SingleOutputFile == True :
+            segname = f"{subdir}/{bookname}{fn.suffix}" # Special Case 1.
+        else:
+            # FILE NAME FORMAT: {num_seq}_{marker information}_({bookname}).mp3    NamingTag
+            segname = f"{subdir}/{segment[0]}_({bookname}){fn.suffix}"
+
         cmd = f"ffmpeg -i -acodec copy -ss {segment[1]} -to {segment[2]}"
+
         command = cmd.split()
-        command.insert(2, filename  )
+        command.insert(2, mp3file_path  )
         command.append( segname   )
 
-        print("command: ",command)
-        print("segment[1]: " + segment[1] + "segment[2]: " + segment[2] + "segname: " +  segname)
         try:
             # ffmpeg requires an output file and so it errors when it does not
             # get one so we need to capture stderr, not stdout.
@@ -109,32 +137,61 @@ def split_file(filename, segments):
             for line in output.splitlines():
                 print(f"Got line: {line}")
 
+# Check if ffmpeg is in directory.
 def isffmpeg(dir):
     if os.path.exists('ffmpeg.exe'):
         return True
     else:
         return False
 
+# Special Case 1
+def isOnly1ItemInList(list):
+    if len(list) == 1:
+        return True
+    else:
+        return False
+
+
 # Get Current directory path
 cur_dir =  os.getcwd()
 
-# Check if ffmpeg in current directory. Exits script if ffmpeg is not found in current dir.
+# Check if ffmpeg in current directory. Exits if ffmpeg is not found.
 if (isffmpeg(cur_dir) is False):
     print("Please put ffmpeg.exe in current directory")
     sys.exit()
 
-
-mp3_files_list = []
+# Get all .mp3s in folder
+mp3paths_list = []
 for file in os.listdir(cur_dir):
     if file.endswith(".mp3"):
         path=os.path.join(cur_dir, file)
-        mp3_files_list.append(r"{}".format(path))
+        mp3paths_list.append(r"{}".format(path))
+print("mp3 file list : \n", mp3paths_list)
 
-print("mp3 file list : \n", mp3_files_list)
+# Create Single Directory For all mp3.
+bookname = mp3paths_list[1]
+bookname = pathlib.Path(bookname)
+bookname = bookname.stem
+bookname = bookname[:-7]
+print("Bookname :", bookname)
+subdir = pathlib.Path(bookname)
+
+# Check if bookname subdirectory has already been created.
+if os.path.exists(subdir):
+    input("Folder exists already. Press enter to delete.")
+    shutil.rmtree(subdir)
+    print("subdir exists. Subdir deleted and new subdir created.")
+    subdir.mkdir()
+else:
+    subdir.mkdir()
 
 
-for filename in mp3_files_list:
-    print(filename)
-    end_time, segments = build_segments(filename)
+for mp3file_path in mp3paths_list:
+    end_time, segments = build_segments(mp3file_path)
     segments = complete_segments(segments, end_time)
-    split_file(filename, segments)
+
+    # For Special Case 1. Check if there is only one file.
+    if (isOnly1ItemInList(mp3paths_list) == True):
+        is_SingleInputFile = True
+        
+    split_file(mp3file_path, segments, bookname)
