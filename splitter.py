38703f6cabfb7124207c33aff7bcef1b29edd9e0
Joe Fresh <jggsan2@gmail.com>
2021-01-15 22:51:09 -0700

    Added function to check if ffmpeg is in directory. Small change in output file naming (personal preference).

--- splitter.py
+++ splitter.py
@@ -10,8 +10,11 @@ import shutil
 Removed unneccesary comments.
 Revamped command list creation.
 Don't forget to put ffmpeg in folder you welp dumbass!
+Added function to check if ffmpeg is in directory.
+
 '''
 
+# Converts time (in secs) to a hh:mm:ss.### format
 def convert_time(time_secs):
     fraction = int((time_secs % 1) * 1000)
     seconds = int(time_secs)
@@ -19,6 +22,7 @@ def convert_time(time_secs):
     hour, min = divmod(min, 60)
     return f"{hour:02}:{min:02}:{sec:02}.{fraction:03}"
 
+# Get OverDrive MediaMarkers, seperates each marker with it's time.
 def build_segments(filename):
     audio = eyed3.load(filename)
     end_time = convert_time(audio.info.time_secs)
@@ -27,7 +31,7 @@ def build_segments(filename):
     print("OverDrive MediaMarkers: " , xmltext)
     markers = ET.fromstring(xmltext)
     base_chapter = "invalid I hope I never have chapters like this"
-    chapter_section = 0
+    num_seq = 0
     segments = []
     for marker in markers:
         # chapters can be split into several shorter sections and end up being
@@ -40,15 +44,15 @@ def build_segments(filename):
         #    Chapter 1-01      03:21.507
         #    Chapter 1-02      06:27.227
         name = marker[0].text.strip()
-        # Replaces characters that misses with complete_segments
-        name = name.replace(":", "")
+        # Replaces characters that are prohibited in Windows File Name Conventions
+        name = name.replace(":", "-")
         name = name.replace("/", "_")
         if not name.startswith(base_chapter):
             base_chapter = name
-            chapter_section = 0
-        # The 00 is ignored if chapter_section == 0. (personal preference: Want names to be as short as possible. I didnt like 00 for every book)
-        name = f'{base_chapter}{"_" + str(chapter_section) if chapter_section!=0 else ""}'
-        chapter_section += 1
+
+        # if num_seq != 0 show 0
+        name = f'{"_" + (str(num_seq) if num_seq!=0 else "0") + "_"   }{base_chapter}'
+        num_seq += 1
         start_time =  marker[1].text
         name = name.replace(" ", "_")
         segments.append((name, start_time))
@@ -67,20 +71,34 @@ def complete_segments(segments, final_time):
     return new_segments
 
 def split_file(filename, segments):
+    print("filename: ", filename)
     fn = pathlib.Path(filename)
-    subdir = pathlib.Path(fn.stem)
+    fname = fn.stem
+    # removes last 11 digits. inclding
+    fname = fname[:-7]
+    subdir = pathlib.Path(fname)
     # Gives user a warning before deleting existing folder.
     if os.path.exists(subdir):
-        input("Folder exists already. Press enter to continue.")
+        # input("Folder exists already. Press enter to continue.")
         shutil.rmtree(subdir)
-    subdir.mkdir()
+    else:
+        subdir.mkdir()
+
+    print("subdir: ",subdir)
     for segment in segments:
-        segname = f"{subdir}/{fn.stem}_{segment[0]}{fn.suffix}"
+        print("segments: ",segments)
+        print("segment: ",segment)
+
+        segname = f"{subdir}/{fname}_{segment[0]}{fn.suffix}"
+        print("segname: ",segname)
         # GOT IT. It is the command = cmd.split() that split it all based on "spaces". We need to split cmd and add ["ffmpeg", "-i", "asdf123"] in front into command.
         cmd = f"ffmpeg -i -acodec copy -ss {segment[1]} -to {segment[2]}"
         command = cmd.split()
         command.insert(2, filename  )
         command.append( segname   )
+
+        print("command: ",command)
+        print("segment[1]: " + segment[1] + "segment[2]: " + segment[2] + "segname: " +  segname)
         try:
             # ffmpeg requires an output file and so it errors when it does not
             # get one so we need to capture stderr, not stdout.
@@ -91,16 +109,31 @@ def split_file(filename, segments):
             for line in output.splitlines():
                 print(f"Got line: {line}")
 
-# Get all .mp3 files in current directory
+def isffmpeg(dir):
+    if os.path.exists('ffmpeg.exe'):
+        return True
+    else:
+        return False
+
+# Get Current directory path
 cur_dir =  os.getcwd()
-mp3_files = []
+
+# Check if ffmpeg in current directory. Exits script if ffmpeg is not found in current dir.
+if (isffmpeg(cur_dir) is False):
+    print("Please put ffmpeg.exe in current directory")
+    sys.exit()
+
+
+mp3_files_list = []
 for file in os.listdir(cur_dir):
     if file.endswith(".mp3"):
         path=os.path.join(cur_dir, file)
-        mp3_files.append(r"{}".format(path))
+        mp3_files_list.append(r"{}".format(path))
+
+print("mp3 file list : \n", mp3_files_list)
 
 
-for filename in mp3_files:
+for filename in mp3_files_list:
     print(filename)
     end_time, segments = build_segments(filename)
     segments = complete_segments(segments, end_time)
